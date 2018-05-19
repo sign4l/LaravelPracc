2c27e440e2ec967ab942056800550b891d34c26f
Jordi Dalmau <jdalmau@gmail.com>
2018-05-19 09:10:50 +0200

    First

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Titulo</h2>
+				<p>Posted 12h ago</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Titulo2</h2>
+				<p>Posted 12h ago</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Titulo3</h2>
+				<p>Posted 12h ago</p>
+			</div>
+		</div>
+	<hr>	
+	
+@endsection
---
+++ resources/views/posts/single.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+			<div class="col-md-12">
+				<h2>Titulo</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dicta autem architecto laboriosam expedita, quisquam alias pariatur aperiam perferendis, recusandae consequatur tenetur dolore quasi aut obcaecati perspiciatis sunt impedit fugiat!</p>
+				<p>Posted 12h ago</p>
+			</div>
+		</div>
+	<hr>
+@endsection
+
+	
